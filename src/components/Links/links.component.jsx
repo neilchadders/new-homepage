b52f9bbb21cb95c07513cb425ec39e432c676106
Neil Chadwick <neilchadders@hotmail.com>
2023-08-20 18:39:02 +0100

    add media queries

--- src/components/Links/links.component.jsx
+++ src/components/Links/links.component.jsx
@@ -3,10 +3,10 @@ import "./links.styles.css"
 const Links = () => {
     return (
         <div id="links">
-            <a href='#about' className="link-hov">aboutMe</a>
-            <a href='#about' className="link-hov">someProjects</a>
-            <a href='#projects' className="link-hov">myBlog</a>
-            <a href="blog" className="link-hov">contactMe</a>
+            <a href='#about' className="link-hov">aboutMe <span className="pipe">|</span></a>
+            <a href='#about' className="link-hov">someProjects <span className="pipe">|</span></a>
+            <a href='#projects' className="link-hov">myBlog <span className="pipe">|</span></a>
+            <a href="blog" className="link-hov">contactMe<span className="pipe">|</span></a>
         </div>
     )
 }
