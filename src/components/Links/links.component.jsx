54f87b247f315edf2aef2f64c21eeebaab731dd8
Neil Chadwick <neilchadders@hotmail.com>
2023-08-26 12:20:12 +0100

    adding hamburger menu

--- src/components/Links/links.component.jsx
+++ src/components/Links/links.component.jsx
@@ -7,6 +7,21 @@ const Links = () => {
             <a href='#projects' className="link-hov">someProjects <span className="pipe">|</span></a>
             <a href='#blog' className="link-hov">myBlog <span className="pipe">|</span></a>
             <a href="#contact" className="link-hov">contactMe</a>
+
+            <input type="checkbox" id="hamburger-input" class="burger-shower" />
+            <label id="hamburger-menu" for="hamburger-input">
+                <nav id="sidebar-menu">
+                    <h3>Menu</h3>
+                    <ul>
+                        <li><a href="#">aboutMe</a></li>
+                        <li><a href="#">someProjects</a></li>
+                        <li><a href="#">myBlog</a></li>
+                        <li><a href="#">contactMe</a></li>
+                    </ul>
+                </nav>
+            </label>
+
+            <div class="overlay"></div>
         </div>
     )
 }
