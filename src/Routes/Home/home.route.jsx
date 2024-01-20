9ad3a5ffe5524bf5068134757c590776d03edd9e
Neil Chadwick <neilchadders@hotmail.com>
2024-01-20 09:11:09 +0000

    add noughts and crosses

--- src/Routes/Home/home.route.jsx
+++ src/Routes/Home/home.route.jsx
@@ -1,13 +1,14 @@
 import Header from '../../components/Header/header.component.jsx'
+import Nought from '../../components/NoughtCross/nought.component.jsx'
 import Quote from '../../components/Quote/quote.component.jsx'
-
 
 
 const Home = () =>{
     return (
         <>
         <Header />
-        <Quote />
+        
+        <Nought />
         </>
     )
 }
