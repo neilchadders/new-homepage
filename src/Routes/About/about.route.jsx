0b1bc0623e763d4b550d7b8c505aebfeba8d0ea9
Neil Chadwick <neilchadders@hotmail.com>
2024-01-21 18:05:58 +0000

    placing Type at bottom of aboutMe

--- src/Routes/About/about.route.jsx
+++ src/Routes/About/about.route.jsx
@@ -3,17 +3,16 @@ import "./about.styles.css"
 import Header from '../../components/Header/header.component.jsx'
 import Type from '../../components/Type/type.component.jsx'
 import Bio from '../../components/Bio/bio.component.jsx'
-import Photo from "../../components/Photo/photo.component.jsx"
-//import Photo from '../../components/Photo/photo.component.jsx'
+
 
 
 const About = () =>{
     return (
         <div>
-        <Header className = "header"/>
-        <Type className = "test Type"/>
-        <Photo />
-        <Bio className = "test Bio"/>
+        <Header />
+        <Bio />
+        <Type />
+     
         </div>
        
     )
--- src/components/Type/type.component.jsx
+++ src/components/Type/type.component.jsx
@@ -1,4 +1,4 @@
-import Photo from '../Photo/photo.component';
+
 import './type.styles.css'
 import Typewriter from "typewriter-effect";
 
@@ -9,15 +9,15 @@ const Type = () =>{
 
     return(
         <div id='object'>
-           <Typewriter
+           <Typewriter className = "type-container"
                 onInit={(typewriter) => {
                     typewriter
-                        .typeString( "<span class ='var-highlight'>var</span> aboutMe = {<br>Name: <span className='string-highlight'>'Neil',</span><br>Location: <span className='string-highlight'>'Yorkshire',</span><br>Tech-Used:[<span className='string-highlight'>'Javascript',</span><span className='string-highlight'>'HTML',</span><span className='string-highlight'>'CSS',</span><span className='string-highlight'>'SASS',</span><span className='string-highlight'>'React',</span><span className ='string-highlight'>'PHP',</span><span className='string-highlight'>'Node.js',</span><span className ='string-highlight'>'mySQL'</span>]<br>};")
+                        .typeString( "<span class ='var-highlight'>var</span> aboutMe = {<br>Name: <span className='string-highlight'>Neil,</span><br>Location: <span className='string-highlight'>Yorkshire,</span><br>Tech-Used:[<span className='string-highlight'>JavaScript,</span><span className='string-highlight'>HTML,</span><span className='string-highlight'>CSS,</span><span className='string-highlight'>SASS,</span><span className='string-highlight'>React,</span><span className ='string-highlight'>PHP,</span><span className='string-highlight'>Node.js,</span><span className ='string-highlight'>mySQL</span>]<br>};")
                         .pauseFor(1000)
                         .start()
                 }}
             />
-            <Photo />
+ 
         </div>
                 
          )
