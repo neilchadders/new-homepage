f5fb6e79bbb5f5cbe1ced6d15f939b06f34fddad
Neil Chadwick <neilchadders@hotmail.com>
2024-01-24 06:56:51 +0000

    add form component html and use state

--- src/Routes/Contact/contact.route.jsx
+++ src/Routes/Contact/contact.route.jsx
@@ -1,13 +1,40 @@
 import Header from '../../components/Header/header.component.jsx'
+
 import './contact.styles.css'
 
-
+import { useState } from 'react';
 
 const Contact = () =>{
+    const [email, setEmail] = useState('')
+    const [subject, setSubject] = useState('')
+    const [message, setMessage] = useState('')
     return (
         <>
         <Header />
-        <h1 className='color'>Contact</h1>
+        <div className="App">
+      <section>
+        <form>
+          <h1>Send Email</h1>
+          <div className='form-wrapper'>
+          <div>
+            <label htmlFor='email'>Email Address</label>
+            <input onChange={(e)=>setEmail(e.target.value)} type="email" id="email"></input>
+          </div>
+          <div>
+            <label htmlFor='subject'>Email Subject</label>
+            <input onChange={(e)=>setSubject(e.target.value)} type="text" id="subject"></input>
+          </div>
+          <div>
+            <label htmlFor='message'>Message Body</label>
+            <textarea onChange={(e)=>setMessage(e.target.value)} type="text" id="message"></textarea>
+          </div>
+          <div>
+            <button type='submit'>Send Email</button>
+          </div>
+          </div>
+        </form>
+      </section>
+    </div>
         </>
     )
 }
