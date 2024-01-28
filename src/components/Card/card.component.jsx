1d14395ef8dba9e53d0e6dd7e0f82f0dd7f0432f
Neil Chadwick <neilchadders@hotmail.com>
2024-01-28 09:00:36 +0000

    style card css component

--- src/components/Card/card.component.jsx
+++ src/components/Card/card.component.jsx
@@ -7,15 +7,34 @@ const Card = ({ project}) => {
 
     return (
         <div className="card-container">
-            <img className="pic" src={pic} />
+              <article>
+    <div class="article-wrapper">
+      <figure>
+        <img className="pic" src={pic} alt="" />
+      </figure>
+      <div class="article-body">
+        <h2>This is some title</h2>
+        <p>
+          Curabitur convallis ac quam vitae laoreet. Nulla mauris ante, euismod sed lacus sit amet, congue bibendum eros. Etiam mattis lobortis porta. Vestibulum ultrices iaculis enim imperdiet egestas.
+        </p>
+        <a href="#" class="read-more">
+          Read more <span class="sr-only">about this is some title</span>
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </article>
 
-    
-            <h3 className="name">{name}</h3>
-            <p className="race">{race}</p>
-            <p className='realm'>{realm}</p>
-            <a className="link" href={link} target={"__blank"}>Wiki</a>
+ {/* <div className="card-container">
 
-          
+<img className="pic" src={pic} />
+<h3 className="name">{name}</h3>
+<p className="race">{race}</p>
+<p className='realm'>{realm}</p>
+<a className="link" href={link} target={"_blank"}>Wiki</a>
+    </div> */}
         </div>
     );
 }
