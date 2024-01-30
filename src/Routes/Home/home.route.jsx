b3763789dbf2f4ce33ca53523c47a371ec6c7771
Neil Chadwick <neilchadders@hotmail.com>
2024-01-30 08:19:56 +0000

    new branch picasso mode

--- src/Routes/Home/home.route.jsx
+++ src/Routes/Home/home.route.jsx
@@ -1,15 +1,15 @@
 import Header from '../../components/Header/header.component.jsx'
-//import Nought from '../../components/NoughtCross/nought.component.jsx'
+import './home.styles.css'
 import Quote from '../../components/Quote/quote.component.jsx'
 
 
 const Home = () =>{
     return (
-        <>
+        <div className = "bg">
         <Header />
         <Quote />
        
-        </>
+        </div>
     )
 }
 
