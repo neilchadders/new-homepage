3030f5b1dce70cf4b330495100daa79f8221db48
Neil Chadwick <neilchadders@hotmail.com>
2024-01-30 09:55:22 +0000

    add clean up to use effect

--- src/components/Quote/quote.component.jsx
+++ src/components/Quote/quote.component.jsx
@@ -16,12 +16,13 @@ const Quote = () => {
 
       useEffect(() => {
         document.body.className = theme;
-      }, [theme]);
+        return () => { document.body.className = ''; }
+    });
 
     return (
         <div id="quote" className = {`Home ${'light'}`}>
             <p id="quote-text" className = {`Home ${'light'}`}><em>"Computers are useless. They can only give you answers".</em></p>
-            <p id="author" className = {`Home ${'light'}`}> - Pablo Picasso</p>
+            <p id="author" {`Home ${'light'}`}> - Pablo Picasso</p>
             <button onClick={toggleTheme}>Toggle Theme</button>
         </div>
         
