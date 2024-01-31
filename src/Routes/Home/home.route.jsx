b3ec8f0085b6ad6d919d102ddead69692a7bffc8
Neil Chadwick <neilchadders@hotmail.com>
2024-01-31 16:56:30 +0000

    add dark mode in use context

--- src/Routes/Home/home.route.jsx
+++ src/Routes/Home/home.route.jsx
@@ -1,7 +1,7 @@
 import Header from '../../components/Header/header.component.jsx'
 import './home.styles.css'
 import Quote from '../../components/Quote/quote.component.jsx'
-import { useState, useEffect } from 'react'
+import { DarkModeProvider } from '../../context/DarkModeContext';
 
 
 
@@ -9,27 +9,13 @@ import { useState, useEffect } from 'react'
 
 const Home = () =>{
 
-    const [theme, setTheme] = useState('light') // default set to light 
-
-    const toggleTheme = () => {
-        if (theme === 'light') {
-          setTheme('dark');
-        } else {
-          setTheme('light');
-        }
-      };
-
-      useEffect(() => {
-        document.body.className = theme;
-      }, [theme]);
-
     return (
        
-        <div className = {`Home ${'light'}`}>
-        <Header />
-        <Quote />
-        <button onClick={toggleTheme}>Toggle Theme</button>
-       
+        <div className = "home">
+            <DarkModeProvider>
+                <Header />
+                <Quote />
+            </DarkModeProvider>   
         </div>
     )
 }
--- src/components/Quote/quote.component.jsx
+++ src/components/Quote/quote.component.jsx
@@ -1,14 +1,18 @@
 import "./quote.styles.css"
 //import { useState, useEffect } from "react";
-
+import React, {useContext} from 'react';
+import Lightswitch from '../Lightswitch/lightswitch.component';
+import {DarkModeContext} from '../../context/DarkModeContext';
 
 const Quote = () => {
-
+    const {darkMode} = useContext(DarkModeContext);
 
     return (
-        <div id="quote" className = {`${'light'}`}>
-            <p id="quote-text"><em>"Computers are useless. They can only give you answers".</em></p>
-            <p id="author"> - Pablo Picasso</p>
+        <div className={darkMode ? `Container Container-dark` : `Container Container-light`}>
+            <p id="quote-text" className={darkMode ? `Container Container-dark` : `Container Container-light`}><em>"Computers are useless. They can only give you answers".</em></p>
+            <p id="author" className={darkMode ? `Container Container-dark` : `Container Container-light`}> - Pablo Picasso</p>
+
+            <Lightswitch />
             
         </div>
         
