f48b5317075207942508435b2a7fd88dfb6a2160
Neil Chadwick <neilchadders@hotmail.com>
2025-04-07 17:59:58 +0100

    amend card img size

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,14 @@ import {getFirestore} from "firebase/firestore"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDizvNHlu8EdAJC3yeR1KiFT7Ra6TriYrU",
-    authDomain: "new-homepage-fe120.firebaseapp.com",
-    projectId: "new-homepage-fe120",
-    storageBucket: "new-homepage-fe120.appspot.com",
-    messagingSenderId: "712704037157",
-    appId: "1:712704037157:web:71fe40a67b2bda2955cacc",
-    measurementId: "G-WWMMKM1PY3"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
