162373390d1dbb202d51b92e93618b8db1cea5a6
Neil Chadwick <neilchadders@hotmail.com>
2025-04-07 18:52:09 +0100

    add run build

--- src/components/ContactForm/contactForm.component.jsx
+++ src/components/ContactForm/contactForm.component.jsx
@@ -2,9 +2,9 @@ import {collection, addDoc} from "firebase/firestore";
 import { db} from "../../config/firebase";
 
 import { useState } from "react";
-//import { toast, ToastContainer } from "react-toastify";
-//import 'react-toastify/dist/ReactToastify.css';
-//import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 import {DarkModeContext} from '../../context/DarkModeContext'
 import { useContext } from 'react'
@@ -21,19 +21,28 @@ const ContactForm = () =>{
   const [message, setMessage] = useState("");
 
 
-  const onSubmitForm = async () => {
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
     try {
       await addDoc(contactFormCollectionRef, {
         email: email,
         subject: subject,
         message: message,
       });
-     
+  
+      toast.success("Message sent successfully!");
+  
+      // Clear the form
+      setEmail("");
+      setSubject("");
+      setMessage("");
+  
     } catch (err) {
       console.error(err);
+      toast.error("Failed to send message. Try again.");
     }
   };
-
+  
   const {darkMode} = useContext(DarkModeContext);
   return(
     
@@ -76,7 +85,15 @@ const ContactForm = () =>{
           </div>
           </form>
       </section>
+      <>
+    <ToastContainer position="top-right" autoClose={3000} />
+    <div id="contact-form" className={darkMode ? `Container-light`: `Container-dark`}>
     </div>
+  </>
+
+    </div>
+
+    
       
   );
 }
