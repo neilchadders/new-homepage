57460ed3b324e8a013a9ea05c155d182902a2a78
Neil Chadwick <neilchadders@hotmail.com>
2025-04-17 06:14:35 +0100

    add project pages

--- src/Routes/Projects/projects.js
+++ src/Routes/Projects/projects.js
@@ -9,7 +9,7 @@ const projectArr =
             name: "MERN Stack Ecommerce app",
             tech: "React, CSS, Express, MongoDB, Node.js, Boostrap, nodemailer",
             description: " This is a MERN stack ecommerce application that allows users to browse and purchase chiili sauces. It features login and registeroptions, secure payment through PayPal, and an admin dashboard for managing products and orders. Registering and payments are confirmed by email sent through nodemailer.",
-            link: "http://localhost:3000/projectOne",
+            link: "https://new-homepage-235u.onrender.com/projectOne",
             id: '123abc'
         },
 
@@ -18,7 +18,7 @@ const projectArr =
             name: "Exercise Countdown",
             tech: "Vanilla JS, CSS, HTML", 
             description: "I really wanted to focus on the fundamentals so opted for a vanilla JS project that incorportated async programming and promises. This is a countdown timer that allows you to add or remove exercises, set a time and then start the countdown. It also has a pause and reset function.",
-            link: "http://localhost:3000/projectTwo",
+            link: "https://new-homepage-235u.onrender.com/projectTwo",
             id: "321ABC"
         },
 
@@ -29,7 +29,7 @@ const projectArr =
             name: "My Portfolio site",
             tech: "React, CSS, FireBase, HTML", 
             description: "This is my portfolio site that I built using React and Firebase. It features a home page, projects page, about me page and a contact form. The contact form uses Firebase to send emails to me when someone fills it out. It also incorporates a Darkmode toggle using the useContext hook.",
-            link: "http://localhost:3000/projectThree",
+            link: "https://new-homepage-235u.onrender.com/projectThree",
             id: '1abcsrqrvv98'
         }, ]
       /*
