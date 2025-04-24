9cce67eebaaa5d4610e52413a39080084be6ad72
Neil Chadwick <neilchadders@hotmail.com>
2025-04-24 17:30:12 +0100

    explain project one and teo

--- src/Projects/ProjectThree/projectThree.jsx
+++ src/Projects/ProjectThree/projectThree.jsx
@@ -18,12 +18,13 @@ const ProjectThree = () => {
                         <p>A page to show my learning and projects</p>
                         <p>React and Firebase</p>
                         <Link className = "project-address"to="https://new-homepage-235u.onrender.com/" >It's the site you're on</Link>
+                        <p></p><Link className = "project-address" to = "https://github.com/neilchadders/new-homepage" >View Code</Link>
                         </div>
 
             <div className="container">
                 <div className='project'>
                     <p className='project-text'>
-                        TETS Lorem ipsum dolor sit amet consectetur adipisicing elit...
+                        For my personal site I opted to use React with React Router and Firebase to capture emails. Then to make it more accessible I added a dark mode using the useContect hook.
                     </p>
                 </div>
                 <div className="project photo-div">
@@ -33,11 +34,13 @@ const ProjectThree = () => {
 
             <div className="container">
                 <div className="project photo-div">
-                <img className="photo-project" src="/img/homepage.png" alt="homepage" />
+                <img className="photo-project" src="/img/context.png" alt="homepage" />
                 </div>
                 <div className='project'>
                     <p className='project-text'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit...
+                    This piece of code creates a context named DarkModeContext that uses createContext(). It defines a DarkModeProvider component that 
+                    uses useState to track whether dark mode is active (false by default = light mode). It also includes a function toggleDarkMode that 
+                    flips the current state. This setup allows any component wrapped in the provider to read or update the dark mode state,
                     </p>
                 </div>
             </div>
@@ -45,11 +48,15 @@ const ProjectThree = () => {
             <div className="container">
                 <div className='project'>
                     <p className='project-text'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit...
+                    This component uses Firebase to collect and store form submissions in a Firestore database. When a user fills out the contact form and clicks 
+                    the send button, the onSubmitForm function is triggered. This function prevents the default form behavior and uses the addDoc method from Firebase 
+                    to add a new document containing the email, subject, and message fields to the "contactForm" collection in the Firestore database. 
+                    If the submission is successful, it displays a toast notification and clears the form inputs; if it fails, 
+                    it shows an error message. Firebase is initialized via an imported db configuration, which connects this form to the appropriate Firestore instance.
                     </p>
                 </div>
                 <div className="project photo-div">
-                <img className="photo-project" src="/img/homepage.png" alt="homepage" />
+                <img className="photo-project" src="/img/contactForm.png" alt="homepage" />
                 </div>
             </div>
             <Footer />
--- src/Projects/ProjectTwo/projectTwo.jsx
+++ src/Projects/ProjectTwo/projectTwo.jsx
@@ -18,39 +18,39 @@ const ProjectTwo = () => {
                                     <p>Exercise Countdown App</p>
                                     <p>Vanilla JavaScript, Html and CSS</p>
                                     <Link to="https://hardcore-ritchie-17f9e3.netlify.app/" className='project-address'>Visit Exercise Countdown</Link>
+                                    
+                                    <p></p><Link to = "https://github.com/neilchadders/exercise-countdown-app" className='project-address'>View Code</Link>
                                     </div>
             
 
-            <div className="container">
+                                    <div className="container">
                 <div className='project'>
-                    <p className='project-text'>
-                        TETS Lorem ipsum dolor sit amet consectetur adipisicing elit...
-                    </p>
+                    <div className='project-text'>
+                        <p>This workout timer app enables users to create a customizable circuit by selecting exercises from a predefined list. Users can add or remove exercises individually, select all, or clear their selection entirely. Once exercises are selected, the app reveals options to set a workout duration either by entering a custom time or choosing preset values of 3, 5, or 10 minutes. After setting the time, the app initiates a 3-second countdown ("3, 2, 1, Go!") to prepare the user before the workout begins. During the session, the app displays a new, randomly selected exercise from the user's list every 10 seconds, ensuring variety while preventing immediate repetition of the same activity. The countdown timer updates in real-time, and when time expires, a "Finished!" message is shown.</p>
+                    </div>
                 </div>
                 <div className="project photo-div">
-                    <img className="photo-project" src="/img/exercise.png" alt="exercise" />
+                    <img className="photo-project" src="/img/dom.png" alt="Logo" />
                 </div>
             </div>
 
             <div className="container">
                 <div className="project photo-div">
-                    <img className="photo-project" src="/img/exercise.png" alt="exercise" />
+                    <img className="photo-project" src="/img/product-controller.png" alt="Logo" />
                 </div>
                 <div className='project'>
                     <p className='project-text'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit...
-                    </p>
+                    The app features pause and reset functionality for flexibility during workouts. The pause button toggles the countdown on and off, while the reset button reloads the page to clear all selections and restart the session. Exercises are displayed dynamically using <code>innerHTML</code>, and the app uses <code>addEventListener</code> extensively to handle button interactions for exercise selection, time input, and controls.</p>
+                    </div>
                 </div>
-            </div>
 
             <div className="container">
                 <div className='project'>
                     <p className='project-text'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit...
-                    </p>
+                    Async and Promises are used strategically to coordinate timed events. Specifically, the app uses an <code>async</code> function with <code>await</code> and a <code>Promise</code> to introduce a 3-second delay after the "Start" button is clicked. This allows the initial countdown to play out fully before the main exercise countdown and rotation begin. This use of asynchronous programming ensures smooth sequencing of time-sensitive actions, maintaining a responsive and user-friendly flow throughout the workout experience.</p>
                 </div>
                 <div className="project photo-div">
-                    <img className="photo-project" src="/img/exercise.png" alt="exercise" />
+                    <img className="photo-project" src="/img/promise.png" alt="Logo" />
                 </div>
             </div>
             <Footer />
