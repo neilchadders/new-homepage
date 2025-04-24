eb23c6e6d6dfc588152837b0a8d6b4b7e4cb48d3
Neil Chadwick <neilchadders@hotmail.com>
2025-04-24 18:04:29 +0100

    add current projects

--- src/Routes/Projects/projects.route.jsx
+++ src/Routes/Projects/projects.route.jsx
@@ -7,6 +7,7 @@ import Footer from '../../components/Footer/footer';
 import projectArr from './projects';
 
 import { DarkModeContext } from '../../context/DarkModeContext';
+import { Link } from "react-router-dom";
 
 
 
@@ -16,7 +17,14 @@ const Projects = () =>{
       <div id = "project" className={darkMode ? `Container-light`: `Container-dark`}>
         <Header />
         <CardList projects = {projectArr} className={darkMode ? `Container-light`: `Container-dark`}/>
+        <div className="current-projects">
+          <h2 className="current-title">Current Projects.....</h2>
+          <p className="current-description">A Next.js ecommerce app app with TypeScript, Tailwind, Zod and Postgres. Here is the <Link to = "https://next-app-pi-taupe-62.vercel.app/">site</Link> here is the
+          <Link to = "https://github.com/neilchadders/next-2" className="project-link">Github</Link></p>
+          <p className="project-description">An API using C# and Entity Framework. Please see <Link to = "https://github.com/neilchadders/MyEmptyWebApp" className="project-link">Github</Link></p>
+        </div> 
         <Footer />
+      
       </div>
       
     )
